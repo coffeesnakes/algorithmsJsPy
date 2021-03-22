4c9b5fc73efd973857922757b624b2bf7d1eacff
Jacob Berg <coffeesnakes@gmail.com>
2021-03-21 23:16:34 -0700

    mostRepeated outputs number

---
+++ mostCommonChar.js
@@ -0,0 +1,30 @@
+function mostRepeatedLetter(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+
+  // create character map
+  for (let char of str) {
+    if (charMap[char]) {
+      // increment the character's value if the character existed in the map
+      charMap[char]++;
+    } else {
+      // Otherwise, the value of the character will be increamented by 1
+      charMap[char] = 1;
+    }
+  }
+
+  // find the most commonly used character
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+console.log(charMap)
+  return maxChar;
+}
+
+
+mostRepeatedLetter("I want to rock this interview");
