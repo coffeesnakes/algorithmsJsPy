2306a6e3b0768a11f6cb58bf437d9370a53d98d4
Jacob Berg <coffeesnakes@gmail.com>
2021-04-02 14:51:57 -0700

    fixed anagram to deduct from frequencyCounter pattern per letter

--- bananagram.js
+++ bananagram.js
@@ -5,27 +5,28 @@ Given two strings, write a function to determine if the second string is an anag
 
 
 function isAnagram(string, comparison) {
-	let freq1 = {};
-if (string.length !== comparison.length) {
-	console.log('uneven length, not anagram')
-	return false;
-}
+  let freq1 = {};
+  if (string.length !== comparison.length) {
+    return false;
+  }
 
-for (let i = 0; i < string.length; i++) {
-let letter = string[i];
-if (freq1[letter] ? freq1[letter] += 1 : freq1[letter] = 1);
-}
+  for (let i = 0; i < string.length; i++) {
+    let letter = string[i];
+    if (freq1[letter] ? (freq1[letter] += 1) : (freq1[letter] = 1));
+  }
 
-for (let j = 0; j < comparison.length; j++) {
-	let letter = comparison[j]
-	console.log(freq1);
-	if (!freq1[letter]) {
-		console.log('false');
-		return false;
-	}
-	console.log('true, it is an anagram')
-	return true;
-}
+  for (let j = 0; j < comparison.length; j++) {
+    let letter = comparison[j];
+    console.log(freq1, letter);
+    if (!freq1[letter]) {
+      console.log("false");
+      return false;
+    } else {
+      freq1[letter] -= 1;
+    }
+  }
+	console.log("true");
+  return true;
 }
 
 let str0 = 'cat';
