e48b077914c89d6e2ef9172bbbc4a74bba0972d0
Jacob Berg <coffeesnakes@gmail.com>
2021-04-04 12:05:55 -0700

    refactor lookup with forEach

--- dupLookup.js
+++ dupLookup.js
@@ -1,9 +1,10 @@
+
 var containsDuplicate = function (nums) {
   let lookup = {};
-  for (let i = 0; i < nums.length; i++) {
-    let number = nums[i];
+  nums.forEach((num) => {
+    let number = num;
     lookup[number] ? (lookup[number] += 1) : (lookup[number] = 1);
-  }
+  });
   for (let key in lookup) {
     if (lookup[key] > 1) {
       return true;
@@ -11,3 +12,17 @@ var containsDuplicate = function (nums) {
   }
   return false;
 };
+
+// var containsDuplicate = function (nums) {
+//   let lookup = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     let number = nums[i];
+//     lookup[number] ? (lookup[number] += 1) : (lookup[number] = 1);
+//   }
+//   for (let key in lookup) {
+//     if (lookup[key] > 1) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
