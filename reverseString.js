4f49ab517a87317047375bbd754ce3c6e493325b
Jacob Berg <coffeesnakes@gmail.com>
2021-04-04 12:48:26 -0700

    reverse string js

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+const reverseString = function (string) {
+	return string.split('').reverse().join('');
+}
+
+const str0 = 'hello friend';
+
+const str1 = 'tobor rM';
+
+console.log(reverseString(str0));
+console.log(reverseString(str1));
