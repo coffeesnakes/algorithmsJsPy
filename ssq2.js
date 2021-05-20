205f1e6628228b3b3bccbd51b8a8ef5bfabfa098
Jacob Berg <coffeesnakes@gmail.com>
2021-05-20 10:35:45 -0700

    SSQ improved js

---
+++ ssq2.js
@@ -0,0 +1,19 @@
+function sortedSquaredArray(array) {
+  const sortedSquares = new Array(array.length).fill(0);
+  let smallerValueIdx = 0;
+  let largerValueIdx = array.length - 1;
+
+  for (let idx = array.length - 1; idx >= 0; idx--) {
+    const smallerValue = array[smallerValueIdx];
+    const largerValue = array[largerValueIdx];
+    if (Math.abs(smallerValue) >= Math.abs(largerValue)) {
+      sortedSquares[idx] = smallerValue * smallerValue;
+      smallerValueIdx++;
+    } else {
+      sortedSquares[idx] = largerValue * largerValue;
+      largerValueIdx--;
+    }
+  }
+
+  return sortedSquares;
+}
